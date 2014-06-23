97525e4370fdc9eb4aac8d94fd4a7739fc60e46b
Roozbeh Pournader <roozbeh@google.com>
2014-06-23 05:52:18 +0000

    Add tools for generating sample text.
    git-svn-id: http://noto.googlecode.com/svn/trunk@267 37e447bf-a746-2f79-0798-35135ca55e94

---
+++ tools/generate_sample_texts.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+./generate_sample_text.py 10B00-10B35 > ../sample_texts/und-Avst.txt
+./generate_sample_text.py 1B05-1B33 > ../sample_texts/und-Bali.txt
+./generate_sample_text.py A6A0-A6EF > ../sample_texts/und-Bamu.txt
+./generate_sample_text.py 0995-09A8 09AA-09B0 09B2 09B6-09B9 09F0 09F1 09E6-09EF > ../sample_texts/und-Beng.txt
+./generate_sample_text.py 1401-145F > ../sample_texts/und-Cans.txt
+./generate_sample_text.py 2C80-2CB1 > ../sample_texts/und-Copt.txt
+./generate_sample_text.py 0410-044F > ../sample_texts/und-Cyrl.txt
+./generate_sample_text.py 0915-0939 > ../sample_texts/und-Deva.txt
+./generate_sample_text.py 10400-1044F > ../sample_texts/und-Dsrt.txt
+./generate_sample_text.py 13000-1303F > ../sample_texts/und-Egyp.txt
+./generate_sample_text.py 1200-1248 > ../sample_texts/und-Ethi.txt
+./generate_sample_text.py 10D0-10F0 > ../sample_texts/und-Geor.txt
+./generate_sample_text.py 0391-03A1 03A3-03A9 03B1-03C9 > ../sample_texts/und-Grek.txt
+./generate_sample_text.py 05D0-05EA > ../sample_texts/und-Hebr.txt
+./generate_sample_text.py A984-A9B2 > ../sample_texts/und-Java.txt
+./generate_sample_text.py 0C95-0CA8 0CAA-0CB3 0CB5-0CB9 > ../sample_texts/und-Knda.txt
+./generate_sample_text.py 10000-1000B 10080-1009F > ../sample_texts/und-Linb.txt
+./generate_sample_text.py 1820-1842 > ../sample_texts/und-Mong.txt
+./generate_sample_text.py ABC0-ABE2 > ../sample_texts/und-Mtei.txt
+./generate_sample_text.py 1000-1021 1023-1027 1029-102A > ../sample_texts/und-Mymr.txt
+./generate_sample_text.py 07CA-07E7 07C0-07C9 > ../sample_texts/und-Nkoo.txt
+./generate_sample_text.py A882-A8B3 > ../sample_texts/und-Saur.txt
+./generate_sample_text.py 0D85-0D96 0D9A-0DB1 0DB3-0DBB 0DBD 0DC0-0DC6 > ../sample_texts/und-Sinh.txt
+./generate_sample_text.py 1B83-1BA0 > ../sample_texts/und-Sund.txt
+./generate_sample_text.py 0710-072F > ../sample_texts/und-Syrc.txt
+./generate_sample_text.py 0B95 0B99-0B9A 0B9C 0B9E-0B9F 0BA3-0BA4 0BA8-0BAA 0BAE-0BB9 > ../sample_texts/und-Taml.txt
+./generate_sample_text.py 0C15-0C28 0C2A-0C39 > ../sample_texts/und-Telu.txt
+./generate_sample_text.py 2D30-2D67 > ../sample_texts/und-Tfng.txt
+./generate_sample_text.py 0E01-0E2E > ../sample_texts/und-Thai.txt
+./generate_sample_text.py 10380-1039D 1039F > ../sample_texts/und-Ugar.txt
+./generate_sample_text.py 12000-1202F > ../sample_texts/und-Xsux.txt
