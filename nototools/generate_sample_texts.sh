9c3374b0853882df41dc501c3a3a1c9f0a4e63b8
Roozbeh Pournader <roozbeh@google.com>
2014-09-13 11:10:23 -0700

    Add more extra locale data and sample text ranges.
    Should fix issue 135.

--- nototools/generate_sample_texts.sh
+++ nototools/generate_sample_texts.sh
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+./generate_sample_text.py 30A0-30FF 31F0-31FF > ../sample_texts/ain-Jpan.txt
+
 ./generate_sample_text.py 10B00-10B35 > ../sample_texts/und-Avst.txt
 ./generate_sample_text.py 1B05-1B33 > ../sample_texts/und-Bali.txt
 ./generate_sample_text.py A6A0-A6EF > ../sample_texts/und-Bamu.txt
@@ -27,22 +29,28 @@
 ./generate_sample_text.py 1200-1248 > ../sample_texts/und-Ethi.txt
 ./generate_sample_text.py 10D0-10F0 > ../sample_texts/und-Geor.txt
 ./generate_sample_text.py 0391-03A1 03A3-03A9 03B1-03C9 > ../sample_texts/und-Grek.txt
+./generate_sample_text.py 4E00-4E20 > ../sample_texts/und-Hans.txt
+./generate_sample_text.py 4E00-4E40 > ../sample_texts/und-Hant.txt
 ./generate_sample_text.py 05D0-05EA > ../sample_texts/und-Hebr.txt
 ./generate_sample_text.py A984-A9B2 > ../sample_texts/und-Java.txt
+./generate_sample_text.py A90A-A925 A900-A909 > ../sample_texts/und-Kali.txt
 ./generate_sample_text.py 0C95-0CA8 0CAA-0CB3 0CB5-0CB9 > ../sample_texts/und-Knda.txt
 ./generate_sample_text.py 1A20-1A4C > ../sample_texts/und-Lana.txt
 ./generate_sample_text.py 0E81 0E82 0E84 0E87 0E88 0E8A 0E8D 0E94-0E97 0E99-0E9F 0EA1-0EA3 0EA5 0EA7 0EAA 0EAB 0EAD 0EAE > ../sample_texts/und-Laoo.txt
 ./generate_sample_text.py 1C00-1C23 > ../sample_texts/und-Lepc.txt
 ./generate_sample_text.py 10000-1000B 10080-1009F > ../sample_texts/und-Linb.txt
+./generate_sample_text.py 0840-0858 > ../sample_texts/und-Mand.txt
 ./generate_sample_text.py 1820-1842 > ../sample_texts/und-Mong.txt
 ./generate_sample_text.py ABC0-ABE2 > ../sample_texts/und-Mtei.txt
 ./generate_sample_text.py 1000-1021 1023-1027 1029-102A > ../sample_texts/und-Mymr.txt
 ./generate_sample_text.py 07CA-07E7 07C0-07C9 > ../sample_texts/und-Nkoo.txt
+./generate_sample_text.py 10C00-10C48 > ../sample_texts/und-Orkh.txt
 ./generate_sample_text.py A882-A8B3 > ../sample_texts/und-Saur.txt
 ./generate_sample_text.py 0D85-0D96 0D9A-0DB1 0DB3-0DBB 0DBD 0DC0-0DC6 > ../sample_texts/und-Sinh.txt
 ./generate_sample_text.py 1B83-1BA0 > ../sample_texts/und-Sund.txt
 ./generate_sample_text.py 0710-072F > ../sample_texts/und-Syrc.txt
 ./generate_sample_text.py 0B95 0B99-0B9A 0B9C 0B9E-0B9F 0BA3-0BA4 0BA8-0BAA 0BAE-0BB9 > ../sample_texts/und-Taml.txt
+./generate_sample_text.py AA80-AAAF AADB-AADF > ../sample_texts/und-Tavt.txt
 ./generate_sample_text.py 0C15-0C28 0C2A-0C33 0C35-0C39 > ../sample_texts/und-Telu.txt
 ./generate_sample_text.py 2D30-2D67 > ../sample_texts/und-Tfng.txt
 ./generate_sample_text.py 0E01-0E2E > ../sample_texts/und-Thai.txt
