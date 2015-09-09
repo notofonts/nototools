94edb0f7c4e13882d7cb057980a09156fa8369e0
Doug Felt <dougfelt@google.com>
2015-09-09 14:22:51 -0700

    wip

--- nototools/swat_license.py
+++ nototools/swat_license.py
@@ -71,18 +71,18 @@ def _swat_fonts(dst_root, dry_run):
             font.fmt)
   fonts = noto_fonts.get_noto_fonts()
   for font in sorted(fonts, key=compare_key):
-    _swat_font(font.filepath, dst_root, dry_run)
+    _swat_font(font, dst_root, dry_run)
 
-def _swat_font(filepath, dst_root, dry_run):
+def _swat_font(noto_font, dst_root, dry_run):
+  filepath = noto_font.filepath
   basename, ext = path.splitext(path.basename(filepath))
-  if ext != 'ttf':
-    ttfont = ttLib.TTFont(filepath, fontNumber=0)
-  else:
-    ttfont = ttLib.TTFont(filepath)
+  if noto_font.is_cjk:
+    print '# Skipping cjk font %s' % basename
+    return
+
+  ttfont = ttLib.TTFont(filepath, fontNumber=0)
 
   names = font_data.get_name_records(ttfont)
-  if names[_LICENSE_ID] == _SIL_LICENSE and names[_LICENSE_URL_ID] == _SIL_LICENSE_URL:
-    return
 
   print '-----\nUpdating %s' % filepath
   # create relative root path
@@ -100,7 +100,7 @@ def _swat_font(filepath, dst_root, dry_run):
   version = names[_VERSION_ID]
   m = re.match(r'Version (\d{1,5})\.(\d{1,5})(.*)', version)
   if not m:
-    print 'Could not match version string (%s)' % version
+    print '! Could not match version string (%s)' % version
     return
 
   major_version = m.group(1)
@@ -111,7 +111,7 @@ def _swat_font(filepath, dst_root, dry_run):
   # sanity check
   expected_revision = major_version + '.' + minor_version
   if expected_revision != print_revision:
-    print 'Expected revision \'%s\' but got revision \'%s\'' % (
+    print '! Expected revision \'%s\' but got revision \'%s\'' % (
         expected_revision, print_revision)
     return
 
@@ -128,12 +128,42 @@ def _swat_font(filepath, dst_root, dry_run):
   rt_float_rev_frac = int(round((rt_float_rev - rt_float_rev_int) * 10 ** accuracy))
   rt_new_revision = str(rt_float_rev_int) + '.' + str(rt_float_rev_frac).zfill(accuracy)
   if new_revision != rt_new_revision:
-    print 'Could not update new revision, expected \'%s\' but got \'%s\'' % (
+    print '! Could not update new revision, expected \'%s\' but got \'%s\'' % (
         new_revision, rt_new_revision)
     return
 
-  new_version_string = 'Version ' + new_revision + version_remainder
+  new_version_string = 'Version ' + new_revision;
+  if not noto_font.is_hinted:
+    new_version_string += ' uh'
+  if version_remainder != ' uh':
+    print '# omitting version remainder \'%s\'' % version_remainder
+
   print '%s: %s' % ('Would write' if dry_run else 'Writing', dst_file)
+
+  # vendor url
+  NOTO_URL = "http://www.google.com/get/noto/"
+
+  # trademark message
+  TRADEMARK = "%s is a trademark of Google Inc." % family
+
+  # description field - should be set.  Roozbeh has note, make sure design field has
+  # information on whether the font is hinted.
+  # Missing in Lao and Khmer, default in Cham.
+  if noto_font.script in ['Lao', 'Khmer', 'Cham']:
+    decription = (
+        'Data %shinted. Noto %s is a humanist %s serif typeface designed for user interfaces '
+        'and electronic communication.' % (
+            '' if hinted else 'un',
+            'Sans' if sans else 'Serif',
+            'sans ' if sans else ''))
+  elif noto_font.vendor is 'Monotype':
+    description = (
+      'Data %shinted. Designed by Monotype design team.' % '' if hinted else 'un'))
+  else:
+    print '# could not generate description'
+    description = None
+
+  # Designer name
 
   if dry_run:
     return
@@ -141,6 +171,11 @@ def _swat_font(filepath, dst_root, dry_run):
   font_data.set_name_record(ttfont, _LICENSE_ID, _SIL_LICENSE)
   font_data.set_name_record(ttfont, _LICENSE_URL_ID, _SIL_LICENSE_URL)
   font_data.set_name_record(ttfont, _VERSION_ID, new_version_string)
+  font_data.set_name_record(ttfont, _VENDOR_URL, NOTO_URL)
+  font_data.set_name_record(ttfont, _TRADMARK, TRADEMARK)
+  if description:
+    font_data.set_name_record(ttfont, _DESCRIPTION, description)
+
   ttfont['head'].fontRevision = float_revision
 
   dst_dir = path.dirname(dst_file)
