b8b946832e83e3c50a1dab9dc26472c95792ef07
Doug Felt <dougfelt@google.com>
2016-10-03 13:13:33 -0700

    Update cmap data with additions from Monotype.
    - FE00 is added for LGC and MONO, used for variant zero.
    - 0021, 061f, 204f, 2e41 added to Adlam, font is designed with extra support
    
    Diff report below:
    
    base: 2016-09-28 noto_cmap_reqs mergefile=noto_cmap_phase3.xml
    target: 2016-10-03 noto_cmap_reqs mergefile=noto_cmap_phase3.xml
    Adlm # Adlam
      added (4): 0021 061f 204f 2e41
        Basic Latin
          0021 Zyyy Po 1.1 EXCLAMATION MARK
        Arabic
          061f Zyyy Po 1.1 ARABIC QUESTION MARK (Arab,Syrc,Thaa)
        General Punctuation
          204f Zyyy Po 3.2 REVERSED SEMICOLON
        Supplemental Punctuation
          2e41 Zyyy Po 7.0 REVERSED COMMA
    LGC # (LGC)
      added (1): fe00
        Variation Selectors
          fe00 Zinh Mn 3.2 VARIATION SELECTOR-1
    MONO # MONO
      added (1): fe00
        Variation Selectors
          fe00 Zinh Mn 3.2 VARIATION SELECTOR-1

--- nototools/noto_cmap_reqs.py
+++ nototools/noto_cmap_reqs.py
@@ -884,6 +884,22 @@ def _generate_script_extra(script_to_chars):
 # maintained using 'regen_script_required' fn
 _SCRIPT_REQUIRED = [
   # Adlm - Adlm (Adlam)
+  ('Adlm',
+   # Comment
+   """
+   Additional characters recommended by Monotype.
+   """,
+   # Data
+   """
+   # Basic Latin
+   0021  # EXCLAMATION MARK
+   # Arabic
+   061F  # ARABIC QUESTION MARK
+   # General Punctuation
+   204F  # REVERSED SEMICOLON
+   # Supplemental Punctuation
+   2E41  # REVERSED COMMA
+   """),
 
   # Aghb - Caucasian Albanian
   ('Aghb',
@@ -1618,6 +1634,7 @@ _SCRIPT_REQUIRED = [
   ('LGC',
    # Comment
    """
+   FE00 is for variant zero.
    """,
    # Data
    """
@@ -1663,6 +1680,8 @@ _SCRIPT_REQUIRED = [
    215F  # FRACTION NUMERATOR ONE
    2184  # LATIN SMALL LETTER REVERSED C
    2189  # VULGAR FRACTION ZERO THIRDS
+   # Variation Selectors
+   FE00  # VARIATION SELECTOR-1
    # Specials
    FFFC  # OBJECT REPLACEMENT CHARACTER
    FFFD  # REPLACEMENT CHARACTER
@@ -2677,6 +2696,9 @@ def _assign_mono(cmap_ops):
   cmap_ops.phase('assign cp437 to mono')
   assert cp437_cps != None
   cmap_ops.add_all(cp437_cps, 'MONO')
+
+  # for variant zero
+  cmap_ops.add(0xfe00, 'MONO')
 
   # add APL to MONO as well?:
   # apl_chars = tool_utils.parse_int_ranges('2336-237a 2395')
