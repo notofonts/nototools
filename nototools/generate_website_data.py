74bb24f544a4c94ba138ccf44b9ce032209b0b31
Doug Felt <dougfelt@google.com>
2016-12-20 15:24:04 -0800

    Fix 'python scripts' to locale python using env.
    These scripts used to run python using /usr/bin/python, but this meant
    that running them as executable scripts failed in environments like
    virtualenv.
    
    Also, a bunch of them were not actually executable despite having the
    shebang line.  This makes them all executable.

--- nototools/generate_website_data.py
+++ nototools/generate_website_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 #
 # Copyright 2014 Google Inc. All rights reserved.
--- nototools/update_cldr.py
+++ nototools/update_cldr.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 #
 # Copyright 2015 Google Inc. All rights reserved.
