d39fe5ad92bbad3afcb8ef8890b5e36972764603
Doug Felt <dougfelt@google.com>
2017-01-31 16:12:58 -0800

    Fix typo in previous commit.

--- nototools/noto_lint.py
+++ nototools/noto_lint.py
@@ -996,7 +996,7 @@ def check_font(font_props, filename_error,
           ascent = int(math.ceil(ASCENT * upem / 2048.0))
           descent = int(math.floor(DESCENT * upem / 2048.0))
 
-        is_document = noto_font.style = 'Serif'
+        is_document = noto_font.style == 'Serif'
 
         if tests.check('head/hhea'):
             if not is_document and (font_props.is_UI or deemed_ui):
