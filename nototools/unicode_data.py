6be848c66c43e6845db3f33afb273bf5b1000d28
Doug Felt <dougfelt@google.com>
2017-02-08 12:52:16 -0800

    Fix errors in folded_script_name.
    Got both the regex wrong and the order of arguments wrong, sheesh.

--- nototools/unicode_data.py
+++ nototools/unicode_data.py
@@ -378,12 +378,12 @@ def defined_characters(version=None, scr=None):
   return characters
 
 
-_strip_re = re.compile(r"['-_ ]+")
+_strip_re = re.compile(r"[-'_ ]+")
 def _folded_script_name(script_name):
   """Folds a script name to its bare bones for comparison."""
   # string.translate is changed by codecs, the method no longer takes two
   # parameters and so script_name.translate(None, "'-_ ") fails to compile
-  return _strip_re.sub(script_name, '')
+  return _strip_re.sub('', script_name)
 
 
 def script_code(script_name):
