3ba30cb49a0ee5ac066d02d8322a78008a32dec1
Doug Felt <dougfelt@google.com>
2017-05-19 14:00:21 -0700

    First pass at some Docker tooling.
    There is lots of room for refinement here.
    
    This creates a docker subdir with three image configurations.
    
    'toolbase' sets up the libraries most of noto requires, including
    harfbuzz, cairo, fontconfig, etc.  It is based on Python 2 so is
    much larger than we actually require.
    
    'notobase' pulls in the noto repos, truncating them to a recent
    tagged release in a futile attempt to save some space.  Actually
    though the fonts make these quite large.  Besides these repos
    it also pulls in afdko.  Since some tools expect en-US locale
    it also sets that up.  'notobase' is based on 'toolbase'.
    
    'make_getnoto' is based on 'notobase' and is an implementation of
    the build process for the data behind the get/noto site. It sets up
    the repos at the latest tagged build, generates optipng-compressed
    emoji images (typically for font builds we use zopfli, but it is
    much slower), builds the website samples, and copies the data to
    /tmp/website2.  To use this you need to map this directory in the
    docker filesystem to one on your host before running the image.
    
    The images referred to by the dockerfiles are (currently) not
    named after the directories, unfortunately.  That would make
    setting these up easier.

---
+++ nototools/docker/toolbase/basesetup.sh
@@ -0,0 +1,85 @@
+#! /bin/bash
+set -e
+
+mkdir -p /app/pkgs
+
+# gtk2 for pango, ragel for autogen, gperf for fontconfig, python-lxml for fc build
+apt-get update && apt-get install -y python-gtk2 ragel gperf python-lxml
+
+# install a newer version of git
+GIT="git-2.12.2"
+cd /app/pkgs
+wget https://www.kernel.org/pub/software/scm/git/${GIT}.tar.xz -O - | tar -xJ
+cd $GIT
+make configure
+./configure --prefix=/usr/local
+make all NO_GETTEXT=1
+make install NO_GETTEXT=1
+# update bash cache 
+hash git
+
+# patch lookup path so our python in /usr/local/lib can find pango in /usr/lib
+cat << EOF >> /usr/local/lib/python2.7/sitecustomize.py
+import sys
+sys.path.append('/usr/lib/python2.7/dist-packages')
+sys.path.append('/usr/lib/python2.7/dist-packages/gtk-2.0')
+EOF
+
+# for harfbuzz
+cd /app/pkgs
+git clone https://github.com/behdad/harfbuzz.git
+cd harfbuzz
+# git checkout 1.4.6
+./autogen.sh
+make
+make install
+
+# behdad's cairo has the patch for color emoji
+cd /app/pkgs
+git clone https://github.com/behdad/cairo behdad_cairo
+cd behdad_cairo
+./autogen.sh
+make
+make install
+
+# need new-ish fontconfig to scale bitmap emoji font
+git clone --branch 2.12.1 git://anongit.freedesktop.org/fontconfig
+cd fontconfig
+./autogen.sh --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man
+make
+make install
+
+# install optipng for emoji quick build
+OPTIPNG="optipng-0.7.6"
+cd /app/pkgs
+wget http://prdownloads.sourceforge.net/optipng/${OPTIPNG}.tar.gz -O - | tar -xz
+cd $OPTIPNG
+./configure
+make
+make check
+make install
+
+# install zopflipng for much slower but somewhat better compression.
+# but png can only compress so much.
+cd /app/pkgs
+git clone https://github.com/google/zopfli.git
+cd zopfli
+make zopflipng
+cp zopflipng /usr/local/bin
+
+# get pycairo
+cd /app/pkgs
+git clone git://git.cairographics.org/git/py2cairo
+cd py2cairo
+./waf configure
+./waf build
+./waf install
+
+# ensure libraries get found
+/sbin/ldconfig /usr/local/lib
+
+# at this point we could clean all these repos but perhaps we want to have them in
+# the image for debugging?
+
+echo "DONE"
+
